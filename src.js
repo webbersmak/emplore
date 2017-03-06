60224cd93e2e5e92f691bd1c7ebf5d780fc7b7de
webbersmak <dclemens@gmx.com>
2017-03-06 15:18:51 -0500

    Update src.js

--- src.js
+++ src.js
@@ -1,75 +1,73 @@
-        var emplore = {
-        
-            register: function(name, needs, run) {
+var emplore = {
+    register: function (name, needs, run) {
+        if (!window.container) {
 
-                if (!window.container) {
-                    window.container = (function () {
-
-                        var activeModules = [];
-                        var modulesToActivate = [];
-
-                        function getModuleToActivate(moduleName) {
-                            for (var i = 0; i < modulesToActivate.length; i++) {
-                                if (modulesToActivate[i].name == moduleName) {
-                                    return modulesToActivate[i];
-                                }
-                            }
-                        };
-
-                        function removeModuleToActivate(moduleName) {
-                            for (var i = 0; i < modulesToActivate.length; i++) {
-                                if (modulesToActivate[i].name == moduleName) {
-                                    modulesToActivate.splice(i, 1);
-                                    return;
-                                }
-                            }
-                        };
-
-                        function activateModule(module) {   //all modules have requires
-
-                            var moduleName = module.name;
-
-                            if (activeModules[moduleName]) {
-                                return;
-                            }
-
-                            for (var i = 0; i < module.needs.length; i++) {  //moo
-                                if (!activeModules[module.needs[i]]) {  //moo needs foo
-                                    activateModule(getModuleToActivate(module.needs[i]));  //moo needs foo
-                                }
-                            }
-
-                            var args = [];
-                            for (var i = 0; i < module.needs.length; i++) {
-                                args.push(activeModules[module.needs[i]]);
-                            }
-
-                            activeModules[moduleName] = module.run.apply(this, args);
-                            removeModuleToActivate(moduleName);
-                        };
-
-                        return {
-                            add: function (name, needs, run) {
-                                if (needs.length > 0) {
-                                    modulesToActivate.push({ name: name, needs: needs, run: run });
-                                }
-                                else {
-                                    activeModules[name] = run();
-                                }
-                            },
-                            run: function () {
-                                while (modulesToActivate.length > 0) {
-                                    activateModule(modulesToActivate[0]);
-                                }
-                            }
-                        };
-                    })();
-                }
-
-                window.container.add(name, needs, run);
-            },
-            run: function () {
+            window.onload = function () {
                 window.container.run();
                 delete window.container;
-            }
-        };
+            };
+
+            window.container = (function () {
+                var activeModules = [];
+                var modulesToActivate = [];
+
+                function getModuleToActivate(moduleName) {
+                    for (var i = 0; i < modulesToActivate.length; i++) {
+                        if (modulesToActivate[i].name == moduleName) {
+                            return modulesToActivate[i];
+                        }
+                    }
+                };
+
+                function removeModuleToActivate(moduleName) {
+                    for (var i = 0; i < modulesToActivate.length; i++) {
+                        if (modulesToActivate[i].name == moduleName) {
+                            modulesToActivate.splice(i, 1);
+                            return;
+                        }
+                    }
+                };
+
+                function activateModule(module) {
+                    var moduleName = module.name;
+
+                    if (activeModules[moduleName]) {
+                        return;
+                    }
+
+                    for (var i = 0; i < module.needs.length; i++) {
+                        if (!activeModules[module.needs[i]]) {
+                            activateModule(getModuleToActivate(module.needs[i]));
+                        }
+                    }
+
+                    var args = [];
+                    for (var i = 0; i < module.needs.length; i++) {
+                        args.push(activeModules[module.needs[i]]);
+                    }
+
+                    activeModules[moduleName] = module.run.apply(this, args);
+                    removeModuleToActivate(moduleName);
+                };
+
+                return {
+                    add: function (name, needs, run) {
+                        if (needs.length > 0) {
+                            modulesToActivate.push({ name: name, needs: needs, run: run });
+                        }
+                        else {
+                            activeModules[name] = run();
+                        }
+                    },
+                    run: function () {
+                        while (modulesToActivate.length > 0) {
+                            activateModule(modulesToActivate[0]);
+                        }
+                    }
+                };
+            })();
+        }
+
+        window.container.add(name, needs, run);
+    }
+};
